579d2c889656a90522a67b2499d1a6ce233d9ddf
afalarconm <afalarconm@uc.cl>
2022-06-26 22:36:30 -0400

    5 heroku commit

--- src/config/config.js
+++ src/config/config.js
@@ -28,12 +28,12 @@ const config = {
 }
 };
 
-Object.keys(config).forEach(configkey) => {
+Object.keys(config).forEach((configkey) => {
     const configValue = config[configkey];
     if (configValue.extend) {
         config[configkey] = { ...config[configValue.extend], ...configValue };
     }
-};
+});
 
 module.exports = config;
 
